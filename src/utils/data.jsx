85756e8218d253e65718c8206a3a992cbcecc2d1
Atmesh Pandey <myattitude855@gmail.com>
2025-08-01 21:23:27 +0530

    text change

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,38 +1,38 @@
 const items = [
   {
     image: 'https://media.istockphoto.com/id/2181660906/photo/closeup-top-view-on-violet-and-white-plastic-polypropylene-granules-heap-on-white-backdrop.webp?a=1&b=1&s=612x612&w=0&k=20&c=xySUs3w9xE5JBa3MmdJVyXSwQJrCh9gy8YRpJM12jWg=',
-    title: 'Bioplastic Compound 1',
-    description: 'Eco-friendly compound made from renewable materials.',
+    title: 'PP Polymers (Virgin & Recycled)',
+    description: 'Versatile plastic resin suitable for durable and eco-friendly applications',
     customClass: 'top-6 left-6 text-left',
   },
   {
     image: 'https://media.istockphoto.com/id/2174570617/photo/closeup-top-view-on-blue-plastic-polypropylene-granules-heap-on-blue-backdrop-masterbatch.webp?a=1&b=1&s=612x612&w=0&k=20&c=OC4oH9_6jF0LloIFBZgUDpLR_lz9OzPbm0DZN7wgfTQ=',
-    title: 'Bioplastic Compound 2',
-    description: 'Sustainable plastic alternative reducing waste.',
+    title: 'LDPE Polymers (Virgin & Recycled)',
+    description: 'Flexible polymer ideal for packaging, films, and lightweight plastic products',
     customClass: 'bottom-6 right-6 text-right',
   },
   {
     image: 'https://media.istockphoto.com/id/2179500723/photo/closeup-top-view-on-colorful-plastic-polypropylene-granules-heap-on-white-backdrop.webp?a=1&b=1&s=612x612&w=0&k=20&c=UOUhW373BkPLBXDh_PCGHttYys3BLg_MEzVaCgD6XeY=',
-    title: 'Bioplastic Compound 3',
-    description: 'Innovative green material for a better planet.',
+    title: 'Calcium-Based Filler Masterbatch',
+    description: 'Enhances strength and reduces cost in plastic production processes.',
     customClass: 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center',
   },
   {
     image: 'https://media.istockphoto.com/id/2175413363/photo/top-view-of-colorful-plastic-polypropylene-granules-pouring-from-the-glass-on-a-black.webp?a=1&b=1&s=612x612&w=0&k=20&c=D8UkN2-FIZI4tbVq3tbu23fIv_rY5M8Hc__UAbM0Qk0=',
-    title: 'Bioplastic Compound 4',
-    description: 'Supporting circular economy with bio-compounds.',
+    title: 'Calcium Carbonate Masterbatch',
+    description: 'Widely used mineral filler for improved plastic stiffness and opacity.',
     customClass: 'top-8 right-8 text-right',
   },
   {
     image: 'https://media.istockphoto.com/id/1864515239/photo/white-plastic-grain-plastic-polymer-granules-hand-hold-polymer-pellets-raw-materials-for.webp?a=1&b=1&s=612x612&w=0&k=20&c=yVvnMrzDzBoXGiCxOWdRflD1G13Bm1TNiCE_EFdiIkI=',
-    title: 'Bioplastic Compound 5',
-    description: 'Durable, eco-conscious plastic compound.',
+    title: 'Triply Grilles',
+    description: 'Durable multi-layer grilles designed for even heat distribution.',
     customClass: 'bottom-8 left-8 text-left',
   },
   {
     image: 'https://media.istockphoto.com/id/2173084546/photo/top-view-of-colorful-plastic-polypropylene-granules-pouring-from-the-glass-on-a-black.webp?a=1&b=1&s=612x612&w=0&k=20&c=D3gP8Q8YxBh6qHZcIutAR92lSXXb3Md365jgsEgV8jQ=',
-    title: 'Bioplastic Compound 6',
-    description: 'Advancing sustainability in materials science.',
+    title: 'Tissue Paper',
+    description: 'Soft, absorbent tissue paper for hygiene and everyday convenience.',
     customClass: 'top-4 left-1/2 transform -translate-x-1/2 text-center',
   }
 ];
