e05b072a61b98544f6b5f31c6ee165bbb5bf987b
seraj-b2 <serajkhan@bamboobox.ai>
2025-08-02 00:59:43 +0530

    Update HeartParts.jsx

--- src/components/HeartParts.jsx
+++ src/components/HeartParts.jsx
@@ -8,7 +8,7 @@ const HeartParts = ({ title, description, image, index }) => {
       <div className="overflow-hidden rounded-t-lg">
         <img src={image} alt="Heart Image" className="w-full h-112 object-cover transform scale-100 transition-transform duration-500 group-hover:scale-120" />
       </div>
-      <div className={`absolute inset-0 flex flex-col ${isEven ? "justify-end" : "justify-start"} p-8 from-black/70 to-transparent`}>
+      <div className={`absolute inset-0 flex flex-col ${isEven ? "justify-end" : "justify-start"} p-8 from-black/70 to-transparent`} style={{ textShadow: "0 0 14px black" }}>
         <h3 className="text-4xl font-bold text-white text-start mb-1">{title}</h3>
 
         <p className="text-xl text-white text-start opacity-0 translate-y-4 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0">{description}</p>
