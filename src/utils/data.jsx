112df8fc5924a5baf462dad26b67238be68cc695
Atmesh Pandey <myattitude855@gmail.com>
2025-08-01 22:12:51 +0530

    remove customclass

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -3,37 +3,51 @@ const items = [
     image: 'https://media.istockphoto.com/id/2181660906/photo/closeup-top-view-on-violet-and-white-plastic-polypropylene-granules-heap-on-white-backdrop.webp?a=1&b=1&s=612x612&w=0&k=20&c=xySUs3w9xE5JBa3MmdJVyXSwQJrCh9gy8YRpJM12jWg=',
     title: 'PP Polymers (Virgin & Recycled)',
     description: 'Versatile plastic resin suitable for durable and eco-friendly applications',
-    customClass: 'top-6 left-6 text-left',
   },
   {
     image: 'https://media.istockphoto.com/id/2174570617/photo/closeup-top-view-on-blue-plastic-polypropylene-granules-heap-on-blue-backdrop-masterbatch.webp?a=1&b=1&s=612x612&w=0&k=20&c=OC4oH9_6jF0LloIFBZgUDpLR_lz9OzPbm0DZN7wgfTQ=',
     title: 'LDPE Polymers (Virgin & Recycled)',
     description: 'Flexible polymer ideal for packaging, films, and lightweight plastic products',
-    customClass: 'bottom-6 right-6 text-right',
   },
   {
     image: 'https://media.istockphoto.com/id/2179500723/photo/closeup-top-view-on-colorful-plastic-polypropylene-granules-heap-on-white-backdrop.webp?a=1&b=1&s=612x612&w=0&k=20&c=UOUhW373BkPLBXDh_PCGHttYys3BLg_MEzVaCgD6XeY=',
     title: 'Calcium-Based Filler Masterbatch',
     description: 'Enhances strength and reduces cost in plastic production processes.',
-    customClass: 'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center',
   },
   {
     image: 'https://media.istockphoto.com/id/2175413363/photo/top-view-of-colorful-plastic-polypropylene-granules-pouring-from-the-glass-on-a-black.webp?a=1&b=1&s=612x612&w=0&k=20&c=D8UkN2-FIZI4tbVq3tbu23fIv_rY5M8Hc__UAbM0Qk0=',
     title: 'Calcium Carbonate Masterbatch',
     description: 'Widely used mineral filler for improved plastic stiffness and opacity.',
-    customClass: 'top-8 right-8 text-right',
   },
   {
     image: 'https://media.istockphoto.com/id/1864515239/photo/white-plastic-grain-plastic-polymer-granules-hand-hold-polymer-pellets-raw-materials-for.webp?a=1&b=1&s=612x612&w=0&k=20&c=yVvnMrzDzBoXGiCxOWdRflD1G13Bm1TNiCE_EFdiIkI=',
     title: 'Triply Grilles',
     description: 'Durable multi-layer grilles designed for even heat distribution.',
-    customClass: 'bottom-8 left-8 text-left',
+  },
+  {
+    image: 'https://media.istockphoto.com/id/2173084546/photo/top-view-of-colorful-plastic-polypropylene-granules-pouring-from-the-glass-on-a-black.webp?a=1&b=1&s=612x612&w=0&k=20&c=D3gP8Q8YxBh6qHZcIutAR92lSXXb3Md365jgsEgV8jQ=',
+    title: 'Triply Utensils',
+    description: 'Soft, absorbent tissue paper for hygiene and everyday convenience.',
+  },
+    {
+    image: 'https://media.istockphoto.com/id/2175413363/photo/top-view-of-colorful-plastic-polypropylene-granules-pouring-from-the-glass-on-a-black.webp?a=1&b=1&s=612x612&w=0&k=20&c=D8UkN2-FIZI4tbVq3tbu23fIv_rY5M8Hc__UAbM0Qk0=',
+    title: 'Chilli (Whole) & Other Spices',
+    description: 'Premium-grade spices exported for culinary and medicinal applications worldwide.',
+  },
+  {
+    image: 'https://media.istockphoto.com/id/1864515239/photo/white-plastic-grain-plastic-polymer-granules-hand-hold-polymer-pellets-raw-materials-for.webp?a=1&b=1&s=612x612&w=0&k=20&c=yVvnMrzDzBoXGiCxOWdRflD1G13Bm1TNiCE_EFdiIkI=',
+    title: 'Non-Woven Bags',
+    description: 'Eco-friendly, reusable bags ideal for retail and promotional use.',
   },
   {
     image: 'https://media.istockphoto.com/id/2173084546/photo/top-view-of-colorful-plastic-polypropylene-granules-pouring-from-the-glass-on-a-black.webp?a=1&b=1&s=612x612&w=0&k=20&c=D3gP8Q8YxBh6qHZcIutAR92lSXXb3Md365jgsEgV8jQ=',
     title: 'Tissue Paper',
     description: 'Soft, absorbent tissue paper for hygiene and everyday convenience.',
-    customClass: 'top-4 left-1/2 transform -translate-x-1/2 text-center',
+  },
+    {
+    image: 'https://media.istockphoto.com/id/2173084546/photo/top-view-of-colorful-plastic-polypropylene-granules-pouring-from-the-glass-on-a-black.webp?a=1&b=1&s=612x612&w=0&k=20&c=D3gP8Q8YxBh6qHZcIutAR92lSXXb3Md365jgsEgV8jQ=',
+    title: 'Disposables',
+    description: 'Practical single-use solutions for food service and event needs.',
   }
 ];
 
