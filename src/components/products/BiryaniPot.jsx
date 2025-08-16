7a650c1f264cc2d6c9a271463843901657bf104d
seraj-b2 <serajkhan@bamboobox.ai>
2025-08-16 22:47:30 +0530

    product images

--- src/components/products/BiryaniPot.jsx
+++ src/components/products/BiryaniPot.jsx
@@ -9,7 +9,7 @@ const BiryaniPot = () => {
         {/* Product Image */}
         <div className="md:w-1/2 w-full max-h-[75vh] overflow-hidden">
           <img
-            src="/images/biryaniPoat.jpg"
+            src="/images/biryaniPoat.png"
             alt="Premium Biryani Pot"
             className="w-full h-full object-cover"
             style={{ maxHeight: '90vh' }} // fallback inline style
